c6045f2d1ce3980b417c6d895b3422dfc5c9b77a
Tod Gentille <tod.gentille@gmail.com>
2019-05-31 11:08:07 -0700

    remove root index.js point main to src

--- index.js
+++
@@ -1,5 +0,0 @@
-const logger = require('./src/util/logger')
-logger.info('hello node world!')
-console.log(`The env var is ${process.env.npm_package_version}`)
-// Leaving this outside the /src folder so that creating an npm package is more straight forward
-// and seems to be canonical way this is done by the packages I've looked at.
