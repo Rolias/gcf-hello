e47118c55cd61b105b263d5887fc9cb1a4d23443
Tod Gentille <tod.gentille@gmail.com>
2019-05-31 11:03:04 -0700

    initial manual commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Seems GCF will look for an index.js file when the deploy command is run
+
+exports.loValidator = (req, res) => {
+  res.status(200).send('I got the request')
+}
+
+// deploy via gcloud
+// gcloud  functions deploy loValidator --stage-bucket <need the bucket> --runtime=nodejs10 --trigger-http
